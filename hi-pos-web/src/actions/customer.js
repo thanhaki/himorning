2e00ca251311f6f62bdb6b59b7d1cfc58e028044
thanhaki <50583845+thanhaki@users.noreply.github.com>
2023-11-16 11:04:27 +0700

    Delete hi-pos-web directory

--- hi-pos-web/src/actions/customer.js
+++
@@ -1,9 +0,0 @@
-
-import {
-    SET_SELECTED_CUSTOMER,
-} from "./types";
-
-export const setSelectedCustomer = (data) => ({
-    type: SET_SELECTED_CUSTOMER,
-    payload: data
-});
--- hi-pos-web/src/actions/printer.js
+++
@@ -1,9 +0,0 @@
-
-import {
-    SHOW_FOOTER,
-} from "./types";
-
-export const showFooter = (status) => ({
-    type: SHOW_FOOTER,
-    payload: status
-});
